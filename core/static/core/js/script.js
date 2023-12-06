28f4fa215a1c1cc57975783068084dc9f664bbf9
JonasEmre <y.emretoktas@gmail.com>
2023-12-06 21:35:19 +0300

    v0.003

--- core/static/core/js/script.js
+++ core/static/core/js/script.js
@@ -1,8 +1,15 @@
 function showMenu() {
-    const hamburgerMenu = document.getElementById("hamburger-menu");
-    if (hamburgerMenu.style.display == "none" || hamburgerMenu.style.display == "") {
-        hamburgerMenu.style.display = "block";
-    } else {
-        hamburgerMenu.style.display = "none";
-    }
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const hamburgerLine = document.getElementById("hamburger-line");
+
+  if (
+    hamburgerMenu.style.display == "none" ||
+    hamburgerMenu.style.display == ""
+  ) {
+    hamburgerMenu.style.display = "block";
+    hamburgerLine.className = "hamburger__line rotate-hamburger"
+  } else {
+    hamburgerMenu.style.display = "none";
+    hamburgerLine.className = "hamburger__line"
+  }
 }
